87a19185e2f46873377c874cea5ca113cf05a38b
HRIDAY-SHINDE <84182255+HRIDAY-SHINDE@users.noreply.github.com>
2023-10-26 00:50:12 +0530

    2

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-let string = "";
-let buttons = document.querySelectorAll('.button');4
+const screen = document.getElementById("result");
+function display(result){
+    screen.value += result
+    return result
+}
+function solve(){
+    let x = screen.value
+    let y = eval(x);
+    screen.value = y
+    return y
+}
+function clearScreen(){
+    screen.value = ''
+}
+function back(){
+    screen.value=screen.value.slice(0,-1)
+}
